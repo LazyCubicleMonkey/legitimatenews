e8d89d7aa0d738d2fddd8b82188d48768d8a048c
Jake Jendiam <nofilterman@gmail.com>
2016-05-13 00:08:00 -0400

    Cleanup crap

--- scripts/menuhider.js
+++ scripts/menuhider.js
@@ -20,47 +20,22 @@ $( document ).ready(function() {
 	var enterHandler = function (ev) {
 		var comp = handler.call(null, ev);
 		if(comp) {
-				
-			//if(outerWidth > width) {
-				var articles = $(comp).find(".articles");
-					var width = $(comp).width();
-					var outerWidth = $(comp).outerWidth();
-					var innerWidth = $(comp).innerWidth();
-					/*console.log("Articles width: " + $(articles).width());
-					console.log("Articles inner width: " + $(articles).innerWidth());
-					console.log("Articles outer width: " + $(articles).outerWidth());
-					console.log("Articles scroll width: " + $(articles)[0].scrollWidth);*/
-				//while(outerWidth > width) {
-					console.log("Width: " + width);
-					console.log("Outer width: " + outerWidth);
-					//console.log("Scroll width: " +  $(comp)[0].scrollWidth);
-					console.log("Inner width: " + innerWidth);
-					var curSize = window.getComputedStyle(articles[0], null).getPropertyValue('font-size');
-					var fontSize = parseInt(curSize.substring(0, curSize.length - 2));
-					console.log("Current font size: " + curSize + " font size: " + fontSize);
-					fontSize = 12;
-					for(var i = 0;i < articles.length; i++) {
-						var article = articles[i];
-						console.log("Article width: " +  $(article).width());
-						/*var width = $(article).width();
-						var outerWidth = $(article).outerWidth();
-						var innerWidth = $(article).innerWidth();
-						console.log("xWidth: " + width);
-						console.log("xOuter width: " + outerWidth);
-						console.log("xScroll width: " +  $(article)[0].scrollWidth);
-						console.log("xInner width: " + innerWidth);*/
-						article.style.fontSize = fontSize + "px";
-					}
-					var w = $(comp).width();
-					outerWidth = $(comp).outerWidth();
-					innerWidth = $(comp).innerWidth();
-				//}
-				//console.log(articles);
-				//comp[0].style.fontSize = comp[0].style.fontSize - 5;
-			//}
-			comp.slideToggle(speed);
+            var articles = $(comp).find(".articles");
+            var width = $(comp).width();
+            var outerWidth = $(comp).outerWidth();
+            var innerWidth = $(comp).innerWidth();
+            var curSize = window.getComputedStyle(articles[0], null).getPropertyValue('font-size');
+            var fontSize = parseInt(curSize.substring(0, curSize.length - 2));
+            fontSize = 12;
+            for(var i = 0;i < articles.length; i++) {
+                var article = articles[i];
+                article.style.fontSize = fontSize + "px";
+            }
+            var w = $(comp).width();
+            outerWidth = $(comp).outerWidth();
+            innerWidth = $(comp).innerWidth();
+            comp.slideToggle(speed);
 		}
-
 	};
 	
 	var exitHandler = function (ev) {
